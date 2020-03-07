b60a6e4e06e6ac894b7b793fb74d1c735481e249
Jonathan Buchan <jbuchan12@live.co.uk>
2020-03-07 20:29:47 +0000

    Addition of create directory

--- src/Services/FileService.py
+++ src/Services/FileService.py
@@ -1,10 +1,18 @@
 import glob
+import os
 from shutil import copyfile
 
 #Get all the files in a given directory
 def getFiles(path : str):
     dirs = glob.glob(path + "*.*")
     return dirs
+
+#Create directory with the directory name at the given location
+def createDirectory(path : str, directoryName : str):
+    try:
+        os.mkdir(path + directoryName)
+    except FileExistsError:
+        print(directoryName + " already exists in " + path + " skipping")
 
 #Copies a file from one dir to another
 def copyFile(fileName : str, fromPath : str, toPath : str, overwriteExistingFiles = False):
--- src/main.py
+++ src/main.py
@@ -2,6 +2,6 @@ from Services import FileService
 
 #Get all the files from a given directory
 def main():
-    FileService.copyFile('Weekly Planner.xlsx',"/Users/jbuchan12/Documents/","/Users/jbuchan12/Documents/Source/FileOperations/src/")
+    FileService.createDirectory("/Users/jbuchan12/Documents/Source/FileOperations/src/","example")
 
 main()
